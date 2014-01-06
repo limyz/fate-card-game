1b9d70dd49db703138cf6c5e4624499b9975a0f2
kietecw@gmail.com <kietecw@gmail.com@8e8a5902-c9d9-5446-3fdb-686419d3f4c4>
2014-01-06 23:58:43 +0000

    + Add New Card.cs and Character.cs + Edit some graphic in InGameScreen

---
+++ trunk/WindowsGame1/WindowsGame1/WindowsGame1/Class/Card.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsGame1.Class
+{
+    [Serializable]
+    public class Card
+    {
+        private ContentManager Content;
+        public string CardType;
+        public string CardName;
+        public string CardAsset;
+        public string CardEff;
+        public enum Suit
+        {
+            Heart = 0,
+            Diamond = 1,
+            Club = 2,
+            Spade = 3
+        };
+        Suit cardSuit;
+        public enum Number
+        {
+            Ace = 1,
+            Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, 
+            Seven = 7, Eight = 8, Nine = 9, Ten = 10,
+            Jack = 11, Queen = 12, King = 13
+        }
+        Number cardNumber;
+        public Texture2D texture;
+        public Card(ContentManager Content, string type, string name, string asset, string effect,
+            Suit suit, Number number)
+        {
+            this.Content = Content;
+            this.CardType = type;
+            this.CardName = name;
+            this.CardAsset = asset;
+            this.CardEff = effect;
+            this.cardSuit = suit;
+            this.cardNumber = number;
+        }
+        public void load_texture()
+        {
+            this.texture = Content.Load<Texture2D>("Resource/card/" + CardAsset);
+        }
+    }
+}
