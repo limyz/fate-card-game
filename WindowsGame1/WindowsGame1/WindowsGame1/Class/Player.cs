13b4e1ea0f3f0f84f57f623ed334e3ea5270a80e
kietecw <kietecw@gmail.com>
2014-01-15 12:53:19 +0000

    + Remove load_texture() in Character.cs and add new GetTexture() in InGameScreen.cs for replacement. + Add GraphicIndex value in Player.cs. + Player disconnected will not be removed from room, edit Status value instead. + Now can synchronize character between all players.

--- WindowsGame1/WindowsGame1/WindowsGame1/Class/Player.cs
+++ WindowsGame1/WindowsGame1/WindowsGame1/Class/Player.cs
@@ -15,6 +15,7 @@ namespace WindowsGame1
         Character character1;
         Character character2;
         public Guid id;
+        int graphicIndex;
         public int HandLimit, HandCurrent, MaxHealth, CurrentHealth;
         //Card equipment1, equipment2, equipment3, equipment4;
         public Card[] Equipment = new Card[4];
@@ -49,6 +50,11 @@ namespace WindowsGame1
         {
             get { return address; }
             set { address = value; }
+        }
+        public int GraphicIndex
+        {
+            get { return graphicIndex; }
+            set { graphicIndex = value; }
         }
         #endregion
 
