8b6cb4006d7ae5bf598d74ae63934f36232c70a7
rikimaru101@gmail.com <rikimaru101@gmail.com@8e8a5902-c9d9-5446-3fdb-686419d3f4c4>
2014-01-17 18:51:09 +0000

    +Add CharacterSelectScreen.cs

---
+++ trunk/WindowsGame1/WindowsGame1/WindowsGame1/Screen/CharacterSelectScreen.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace WindowsGame1
+{
+    public class CharacterSelectScreen : Screen
+    {
+        Background bg;
+
+        public CharacterSelectScreen(GraphicsDeviceManager graphics, ContentManager Content, SivEventHandler theEvent, Game1 parent)
+            : base("JoinScreen", theEvent, parent)
+        {
+            bg = new Background(Content.Load<Texture2D>("Resource/Test"), this);
+
+            OnKeysDown += CharacterSelectScreen_OnKeysDown;
+        }
+
+        private void CharacterSelectScreen_OnKeysDown(Keys[] keys)
+        {
+            foreach (Keys k in keys)
+            {
+                if (k == Keys.Escape)
+                    ScreenEvent.Invoke(this, new SivEventArgs(0));
+                return;
+            }
+        }
+
+        public override void Update(GameTime theTime)
+        {
+            base.Update(theTime);
+        }
+
+        public override void Draw(GraphicsDeviceManager graphics, SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend);
+            spriteBatch.DrawString(Game1.font, main_game.mouse_pos.ToString(), new Vector2(0, 0), Color.White);
+            base.Draw(graphics, spriteBatch, gameTime);
+            spriteBatch.End();
+        }
+    }
+}
