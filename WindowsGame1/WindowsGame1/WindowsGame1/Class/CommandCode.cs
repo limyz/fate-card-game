3f71263b3b25db8f12db0b11a86f9e933f69ed12
kietecw <kietecw@gmail.com>
2014-05-07 20:06:04 +0000

    + Add Turn System

--- WindowsGame1/WindowsGame1/WindowsGame1/Class/CommandCode.cs
+++ WindowsGame1/WindowsGame1/WindowsGame1/Class/CommandCode.cs
@@ -47,6 +47,11 @@ namespace WindowsGame1
         Character_Distribute = 12,
 
         //Synchronize Deck send a List of Card as Data1, used to synchronize any ListCard
-        CardList_Synchronize = 13
+        CardList_Synchronize = 13,
+
+        //Player as Data1
+        Change_Turn = 14,
+
+        End_Turn = 15
     };
 }
---
+++ WindowsGame1/WindowsGame1/WindowsGame1/Class/Turn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsGame1
+{
+    [Serializable]
+    public class Turn
+    {
+        public enum Phase
+        {
+            OtherPlayerTurn = 0,
+            Beginning = 1,
+            Judgment = 2,
+            Draw = 3,
+            Action = 4,
+            Discard = 5,
+            End = 6
+        };
+        public Phase phase;
+        public Turn()
+        {
+            this.phase = Phase.OtherPlayerTurn;
+        }
+    }
+}
